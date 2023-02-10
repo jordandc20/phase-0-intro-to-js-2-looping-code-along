9edc0781afb649f40b73e800b87133d71b8fdeba
Diana Jordan <dianacjordan@gmail.com>
2023-02-09 22:05:32 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+
+function writeCards(names,event) {
+const messages = [];
+for (let i=0; i<names.length; i++){
+messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+}
+console.log(messages)
+return messages;
+}
+
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+
+function countDown(integer) {
+let i=integer
+    while (i>=0){
+console.log(i)
+i--;
+    }
+
+}
+countDown(15)
